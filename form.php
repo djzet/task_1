ef41b94e8e57b67fe12745f2209be072291732ec
djzet <silaevegor2003@mail.ru>
2023-07-13 20:42:30 +0700

    task_1

---
+++ form.php
@@ -0,0 +1,49 @@
+<!DOCTYPE <!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+  <div class="w-50 p-3 container text-center">
+    <form action="index.php" method="post">
+      <div class="row mb-3">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Почта</label>
+        <div class="col-sm-10">
+          <input type="email" name="email" class="form-control" id="inputEmail3">
+        </div>
+      </div>
+      <div class="row mb-3">
+        <label for="inputPassword3" class="col-sm-2 col-form-label">Пароль</label>
+        <div class="col-sm-10">
+          <input type="password" name="password" class="form-control" id="inputPassword3">
+        </div>
+      </div>
+      <div class="row mb-3">
+        <label for="inputDate3" class="col-sm-2 col-form-label">Дата</label>
+        <div class="col-sm-10">
+          <input type="date" name="date" class="form-control" id="inputPassword3">
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary w-50">Отправить</button>
+    </form>
+  </div>
+  <script src="" async defer></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+$date = $_POST['date'];
+
+$emailValidate = '#^[a-z0-9]{1,15}@(?:mail\.ru|gmail\.com)$#';
+
+if (preg_match($emailValidate, $email)) {
+    echo '<br>';
+    echo 'Валидация почты прошла';
+    echo '<br>';
+} else {
+    echo '<br>';
+    echo 'Валидация почты не прошла';
+    echo '<br>';
+}
+
+echo '<br>';
+
+$passworsValidate = '#^[a-zA-Z0-9$]{6,}$#';
+
+if (preg_match($passworsValidate, $password)) {
+    echo '<br>';
+    echo 'Валидация пароля прошла';
+    echo '<br>';
+} else {
+    echo '<br>';
+    echo 'Валидация пароля не прошла';
+    echo '<br>';
+}
+
+echo '<br>';
+
+$dateValidate = '#^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1])$#';
+
+if (preg_match($dateValidate, $date)) {
+    echo '<br>';
+    echo 'Валидация даты прошла';
+    echo '<br>';
+} else {
+    echo '<br>';
+    echo 'Валидация даты не прошла';
+    echo '<br>';
+}
+
+echo '<br>';
+
+$data = ['email' => preg_match($emailValidate, $email), 'password' => preg_match($passworsValidate, $password), 'date' => preg_match($dateValidate, $date)];
+switch ($data) {
+    case (!$data['email']):
+        echo '<br>';
+        echo 'Почта не валидна';
+        echo '<br>';
+        break;
+    case (!$data['password']):
+        echo '<br>';
+        echo 'Пароль не валидет';
+        echo '<br>';
+        break;
+    case (!$data['date']):
+        echo '<br>';
+        echo 'Дата не валидна';
+        echo '<br>';
+        break;
+    default:
+        echo '<br>';
+        echo 'Данные валидны';
+        echo '<br>';
+}
